bc4d675f2f98ea4b25418c01130b38a94704db7e
Antonio <toncas@windowslive.com>
2018-02-22 20:05:21 -0800

    [SERVER] set up routing

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const router = require('./instagreenRouter');
 
 const port = 3000;
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use('/instagreen', router);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
--- server/instagreenRouter.js
+++ server/instagreenRouter.js
@@ -1,5 +1,69 @@
 const router = require('express').Router();
 
-router.route('/instagreen').all((req, res, next) => {
-  next();
+router.post('/follow', (req, res) => {
+  // controller.submitFollowRequest
+
+  // return JSON of the target user
 });
+
+router.post('/post', (req, res) => {
+  // controller.addPost
+
+  // return JSON of newly created post
+});
+
+router.get('/user/:user_id', (req, res) => {
+  // controller.getPostsOfUser
+
+  // Return JSON Array of all posts by user
+
+});
+
+router.get('/expolore', (req, res) => {
+  // controller.getAllPosts
+
+  // Return JSON Array of all posts in db
+
+});
+
+router.get('/feed', (req, res) => {
+  // controller.getPostsOfPeopleFollowed
+
+  // Return JSON Array of all posts of people a user follows
+
+});
+
+router.post('/post/like', (req, res) => {
+  // controller.addLike
+
+  // Return Boolean of success or fail
+});
+
+router.post('/comment/:post_id', (req, res) => {
+  // controller.addNewComment
+
+  // Return JSON of the comment
+
+});
+
+router.put('/follow/accept', (req, res) => {
+  // controller.acceptFollow
+
+  // Return boolean of the accept state
+
+});
+
+router.get('/login', (req, res) => {
+  // controller.login
+
+  // Return JSON of the user (minus the password)
+
+});
+
+router.post('/signup', (req, res) => {
+  // controller.signup
+
+  // Return JSON of the user (minus the password)
+
+});
+
