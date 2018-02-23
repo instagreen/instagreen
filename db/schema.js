f3276d05e491ca75e38305f9cf56cae7f920f662
Antonio <toncas@windowslive.com>
2018-02-23 09:19:44 -0800

    [FORMATTING] fixed formatting for db/schema.js

--- db/schema.js
+++ db/schema.js
@@ -1,80 +1,78 @@
 const Schema = {
-    users: { // table
-      id: { //field
-        type: 'increments',
-        nullable: false,
-        primary: true
-      },
-      username: {
-        type: 'string',
-        maxlength: 255,
-        nullable: false,
-        unique: true
-      },
-      password: {
-          type: 'string',
-          maxlength: 255,
-          nullable: false
-      },
-      follower_count:{
-        type: 'integer',
-        nullable: false,
-        unsigned: true
-      },
-      following_count:{
-          type: 'integer',
-          nullable: false,
-          unsigned: true
-      }    
+  users: { // table
+    id: { // field
+      type: 'increments',
+      nullable: false,
+      primary: true,
     },
-    
-    posts: {
-      id: {
-          type: 'increments',
-          nullable: false,
-          primary: true
-      },
-      likes_count: {
-        type: 'integer',
-        nullable: false,
-        unsigned: false
-      }
+    username: {
+      type: 'string',
+      maxlength: 255,
+      nullable: false,
+      unique: true,
     },
-  
-  
-    comments: {
-      id: {
-          type: 'increments',
-          nullable: false,
-          primary: true
-      },
-      text : {
-        type: 'string',
-        nullable: false,
-        maxlength: 255  
-      }
+    password: {
+      type: 'string',
+      maxlength: 255,
+      nullable: false,
     },
-  
-    user_target_relation: {  //relation table
-      id: {
-          type: 'increments',
-          nullable: false,
-          primary: true
-      },
-      isAccepted: { // 0 for follower 1 for following
-        type: 'boolean',
-        nullable: false
-      },
+    follower_count: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
     },
-  
-    likes: {
-      id: {
-          type: 'increments',
-          nullable: false,
-          primary: true
-      },
+    following_count: {
+      type: 'integer',
+      nullable: false,
+      unsigned: true,
+    },
+  },
+  posts: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true,
+    },
+    likes_count: {
+      type: 'integer',
+      nullable: false,
+      unsigned: false,
+    },
+  },
 
+
+  comments: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true,
     },
-  };
-      
+    text: {
+      type: 'string',
+      nullable: false,
+      maxlength: 255,
+    },
+  },
+
+  user_target_relation: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true,
+    },
+    isAccepted: {
+      type: 'boolean',
+      nullable: false,
+    },
+  },
+
+  likes: {
+    id: {
+      type: 'increments',
+      nullable: false,
+      primary: true,
+    },
+
+  },
+};
 module.exports.Schema = Schema;
