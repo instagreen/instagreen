ee8462f7cc57981f56afab70bc0d55050d2440eb
Antonio <toncas@windowslive.com>
2018-02-24 14:04:31 -0800

    [POST COMPONENT] working on logic for following users

--- server/resources/model.js
+++ server/resources/model.js
@@ -11,36 +11,6 @@ const knex = require('knex')({
 });
 
 // Put all db queries here
-module.exports.handleNewLike = (body, callback) => {
-  // THEN
-  knex('likes')
-    .select()
-    .where({
-      user_id: body.user_id,
-      post_id: body.post_id,
-    })
-    .then((entry) => {
-      // if a record in likes table DOES NOT contain user_id and post_id
-      if (!entry.length) {
-        knex('likes')
-        // add a new like record in the likes database
-          .insert({
-            user_id: body.user_id,
-            post_id: body.post_id,
-          })
-          .then(id => knex('likes').select().where('id', id))
-          .then(callback);
-
-        // increment likes_count of the post in the posts table
-        knex('posts').where({ id: body.post_id })
-          .increment('likes_count', 1)
-          .then(() => console.log('WOO'));
-      } else {
-        callback({ message: 'user already liked' });
-      }
-    });
-};
-
 module.exports.addPostToDb = (body, callback) => {
   knex('posts')
     .insert({
@@ -62,6 +32,66 @@ module.exports.addCommentToDb = (body, callback) => {
     })
     .then(id => knex('comments').select().where('id', id))
     .then(callback);
+};
+
+module.exports.handleNewLike = (body, callback) => {
+  knex('likes')
+    .select()
+    .where({
+      user_id: body.user_id,
+      post_id: body.post_id,
+    })
+    .then((entry) => {
+      // if a record in likes table DOES NOT contain user_id and post_id
+      if (!entry.length) {
+        knex('likes')
+          // add a new like record in the likes database
+          .insert({
+            user_id: body.user_id,
+            post_id: body.post_id,
+          })
+          .then(id => knex('likes').select().where('id', id))
+          .then(callback);
+
+        // increment likes_count of the post in the posts table
+        knex('posts').where({ id: body.post_id })
+          .increment('likes_count', 1)
+          .then(() => console.log('Testing'));
+      } else {
+        callback({ message: 'user already liked' });
+      }
+    });
+};
+
+module.exports.handleFollowRequest = (body, callback) => {
+  knex('user_target_relation')
+    .select()
+    .where({
+      user_id: body.user_id,
+      target_id: body.target_id,
+    })
+    .then((entry) => {
+      if (!entry.length) {
+        knex('user_target_relation')
+          .insert({
+            user_id: body.user_id,
+            target_id: body.target_id,
+            isAccepted: false,
+          })
+          .then(id => knex('user_target_relation').select().where('id', id))
+          .then(callback);
+      } else {
+        callback({ message: 'user already requested follow' });
+      }
+    });
+};
+
+module.exports.handleFollowAccept = (body, callback) => {
+
+};
+
+module.exports.handleFollowDecline = (body, callback) => {
+
 };
 
 module.exports.test = (body, callback) => {
