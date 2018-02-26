82264f9d98e51f355bec12508bbe2f4dcc148043
Antonio <toncas@windowslive.com>
2018-02-26 13:48:25 -0800

    [POST COMPONENT] added ui for comment and liking a post

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      feed: ['test', 'test'],
+      feed: [],
       user_id: 0,
     };
   }
--- client/src/components/post.jsx
+++ client/src/components/post.jsx
@@ -1,24 +1,63 @@
 import React from 'react';
 
-const Post = props => (
-  <div className="post-component">
-    <div className="post-component-image">
-      <img alt="test" src="https://images.freeimages.com/images/large-previews/f2c/effi-1-1366221.jpg" height="250" width="250" />
-    </div>
-    <div className="post-component-description">
-      <em><strong><a href="#">Username: </a></strong>Description goes here</em>
-    </div>
-    <div className="post-component-comment-section">
-      <ul>
-        <li><strong><a href="#">Username: </a></strong>Comments go here</li>
-        <li><strong><a href="#">Username: </a></strong>Comments go here</li>
-        <li><strong><a href="#">Username: </a></strong>Comments go here</li>
-      </ul>
-    </div>
-    <div className="post-component-add-comment">
-      <input type="text" name="new-comment" placeholder="Write a comment..." />
-    </div>
-  </div>
-);
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentList: [],
+      comment: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
+  }
+
+  componentDidMount() {
+    // get postID and fetch all comments based on the postID.
+  }
+
+  handleChange(e) {
+    this.setState({
+      comment: e.target.value,
+    });
+  }
+
+  handleEnter(e) {
+    if (e.key === 'Enter') {
+      console.log(this.state.comment);
+    }
+  }
+
+  render() {
+    return (
+      <div className="post-component">
+        <div className="post-component-image">
+          <img alt="test" src="https://images.freeimages.com/images/large-previews/f2c/effi-1-1366221.jpg" height="250" width="250" />
+        </div>
+        <div className="post-component-description">
+          <em><strong><a href="#">Username: </a></strong>Description goes here</em>
+        </div>
+        <div className="post-component-comment-section">
+          <ul>
+            <li><strong><a href="#">Username: </a></strong>Comments go here</li>
+            <li><strong><a href="#">Username: </a></strong>Comments go here</li>
+            <li><strong><a href="#">Username: </a></strong>Comments go here</li>
+          </ul>
+        </div>
+        <div className="post-component-add-comment">
+          <input
+            type="text"
+            onChange={this.handleChange}
+            onKeyDown={this.handleEnter}
+            name="comment"
+            placeholder="Write a comment..." />
+        </div>
+        <div className="post-component-action-buttons">
+          <button>Follow</button>
+          <button>Like</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Post;
