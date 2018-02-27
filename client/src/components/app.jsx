d0c5b9f274ab7546bebddf1ccee72eb03755bd49
Antonio <toncas@windowslive.com>
2018-02-26 16:32:45 -0800

    [REBASE] rebased with profile branch

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -4,12 +4,8 @@ import ReactDOM from 'react-dom';
 import UserList from '../containers/user-list.jsx';
 import UserDetail from '../containers/user-detail.jsx';
 import Feed from './feed.jsx';
-<<<<<<< HEAD
-import { getFeed } from './../apiCaller.js';
-import Profile from './profile.jsx';
-=======
 import apiCaller from './../apiCaller.js';
->>>>>>> [FRONT END] added method to get username from db
+import Profile from './profile.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -40,15 +36,9 @@ class App extends React.Component {
       <div>
         {/* navbar yet to be made */}
         {/* <Navbar /> */}
-<<<<<<< HEAD
-        <h1>Hello from app.jsx</h1>
-        <Feed user_id={this.state.user_id} feed={this.state.feed} />
-        {/* <Profile personalPosts={this.state.personalPosts} user={this.state.user} /> */}
-=======
         <h1>Hello, {this.state.username}</h1>
         <Feed username={this.state.username} user_id={this.state.user_id} feed={this.state.feed} />
 
->>>>>>> [FRONT END] added method to get username from db
       </div>
     );
   }
