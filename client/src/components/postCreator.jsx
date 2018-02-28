fc0db6ebeddda05f6ab70a6795c8946f2eb91678
Antonio <toncas@windowslive.com>
2018-02-28 13:01:59 -0800

    [POST CREATOR] created postCreator.jsx

---
+++ client/src/components/postCreator.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class PostCreator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+
+      <div className="post-creator">
+        CREATE POST GOES HERE
+      </div>
+    );
+  }
+}
+
+export default PostCreator;
