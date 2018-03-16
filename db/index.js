39dea2ae832fe46fd5624caa439e06e327f246e7
fireArthur <thefirearthur@gmail.com>
2018-03-16 11:21:02 -0700

    [AWS] Changes the host name and password to work with AWS RDS

--- db/index.js
+++ db/index.js
@@ -2,9 +2,16 @@
 // const password = require('./config.example.js');
 const mysql = require('mysql');
 
+// module.exports.connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'instagreen',
+//   // password,
+// });
+
 module.exports.connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'instagreen.cg33syvifjip.us-west-1.rds.amazonaws.com',
   user: 'root',
   database: 'instagreen',
-  // password,
+  password: '123456789',
 });
