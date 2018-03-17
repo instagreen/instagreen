dc09fd9e6099cce1c930b6fe76e5822f4fca12e9
fireArthur <thefirearthur@gmail.com>
2018-03-16 18:21:45 -0700

    [Port] Changes port number to 80

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const favicon = require('serve-favicon');
 const { router } = require('./resources/instagreenRouter');
 
-const port = 3000;
+const port = 80;
 const app = express();
 
 app.use(logger('tiny'));
