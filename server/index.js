fd90e680f337cbabc0da27a9e4fa9a0115a17f1a
fireArthur <thefirearthur@gmail.com>
2018-03-16 18:23:56 -0700

    put it back to 3000

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const favicon = require('serve-favicon');
 const { router } = require('./resources/instagreenRouter');
 
-const port = 80;
+const port = 3000;
 const app = express();
 
 app.use(logger('tiny'));
