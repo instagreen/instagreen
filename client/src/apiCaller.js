a6e74457ab61c51504494ca9f1d15a7faa06b282
fireArthur <thefirearthur@gmail.com>
2018-04-15 14:23:11 -0700

    [ENV] adds dotenv import to the api caller

--- client/src/apiCaller.js
+++ client/src/apiCaller.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const SERVER = process.env.REST_SERVER;
 // const SERVER = 'https://instagreen.herokuapp.com/instagreen';
